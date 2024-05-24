063cb5f1a9173ddaed0b4c85244c3903e777e3d6
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-24 10:10:12 +1000

    update

--- aws-eks-fargate/main.tf
+++ aws-eks-fargate/main.tf
@@ -8,7 +8,7 @@ locals {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.11.1"
+  version = "20.2.0"
 
   cluster_name                   = var.cluster_name
   cluster_version                = var.kubernetes_version 
