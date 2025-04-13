e47cae1e7efdc2ddb0517b16c40a2c5f46c5f916
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-14 08:14:44 +1000

    Add providers.tf for AWS EKS and VPC with required providers configuration

---
+++ aws-eks-addon/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  
+required_providers {
+ 
+  kubernetes = {
+    source  = "hashicorp/kubernetes"
+    version = "~> 2.25"
+  }
+
+  time = {
+    source = "hashicorp/time"
+    version = "~> 0.1"
+  }
+  
+
+  helm = {
+    source = "hashicorp/helm"
+    version = "~> 2.12"
+  }
+
+
+}
+}
